14bcac1136070aa390e617b861cb16e7881df250
Mahmud Hossain <shojib.utube@gmail.com>
2019-03-02 21:37:03 -0500

    Adding the inventory system p3

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject inventorySlotsContainer;
+    public Camera fpsCam;
+    public float range = 4f;
+    public GameObject inventoryItemsContainer;
+    public GameObject inventorySlots;
+
+    private Button[] slots;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventorySlotsContainer.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            bool activeState = inventorySlotsContainer.gameObject.activeSelf;
+            inventorySlotsContainer.gameObject.SetActive(!activeState);
+            Cursor.lockState = activeState ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !activeState;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = activeState;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PickupItem();
+        }
+    }
+
+    public void PickupItem()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            if (hit.transform.CompareTag("Item"))
+            {
+                hit.transform.SetParent(inventoryItemsContainer.transform);
+                AddtoInventory(hit.transform);
+                Debug.Log("Hitting: " + hit.transform.name);
+            }
+        }
+    }
+
+    public void AddtoInventory(Transform item)
+    {
+        Debug.Log("Adding to inventory...");
+        slots = inventorySlots.GetComponentsInChildren<Button>();
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            RawImage rImg = slots[i].GetComponentInChildren<RawImage>();
+            if (rImg.texture == null)
+            {
+                rImg.texture = item.GetComponent<RawImage>().texture;
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public RawImage img;
+    public GameObject itemsContainer;
+    public GameObject inventoryItemsContainer;
+
+    public void DropItem()
+    {
+        print("Clicked and adding an item ... ");
+
+        string name = img.texture.name;
+        Transform itemTransform = inventoryItemsContainer.transform.Find(name);
+        
+        if (itemTransform)
+        {
+            Transform playerTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+            itemTransform.SetPositionAndRotation(playerTransform.position, playerTransform.rotation);
+            itemTransform.SetParent(itemsContainer.transform);
+            img.texture = null;
+        }
+    }
+
+}
